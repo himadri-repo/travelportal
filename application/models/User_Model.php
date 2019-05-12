a77286352459cce6399260bb70d287dfeea1a8c5
himadri-repo <himadri.trainer@gmail.com>
2019-05-12 18:34:07 +0530

    admin pages

--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -675,7 +675,7 @@ Class User_Model extends CI_Model
 	public function get_users($companyid) {
 		$arr = array('u.companyid'=>$companyid);
 
-		$this->db->select('u.id,u.user_id,u.name,u.email,u.mobile,u.is_supplier,u.is_customer,u.active,u.type,u.credit_ac,u.is_admin');
+		$this->db->select('u.id,u.user_id,u.name, u.profile_image, u.email, u.mobile, u.password, u.is_supplier, u.is_customer, u.active, u.type, u.credit_ac, u.doj, u.companyid, u.permission, u.is_admin, u.uid');
 		$this->db->from('user_tbl as u');
         $this->db->where($arr);
 		$this->db->order_by("u.name", "ASC");
