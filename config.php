667c38ca979f9b7c7668472578461dabce7c3fc0
Himadri Majumdar <majumdar.himadri@gmail.com>
2019-05-14 16:50:48 +0530

    config changes

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ mysql_connect('localhost','oxyusr','oxy@321-#');
 mysql_select_db('oxytra');
 session_start();
 
-$baseurl="http://localhost:90";
+$baseurl="http://localhost";
 $sql="SELECT * FROM setting_tbl";
 $result=mysql_query($sql);
 $row_top=mysql_fetch_array($result,MYSQL_ASSOC);
